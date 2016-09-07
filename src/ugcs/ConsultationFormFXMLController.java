5ea69d1c6373d8468aa8a9dbc49fc1b2c44204d6
NotoriousCurry <sgaheer95@gmail.com>
2016-09-07 18:05:29 +1000

    fixed some fck ups

--- src/ugcs/ConsultationFormFXMLController.java
+++ src/ugcs/ConsultationFormFXMLController.java
@@ -6,19 +6,21 @@
 package ugcs;
 
 import java.net.URL;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
@@ -37,18 +39,8 @@ public class ConsultationFormFXMLController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        timePicked.getItems().addAll("9am", "10am",
-                "11am",
-                "12pm",
-                "1pm",
-                "2pm",
-                "3pm",
-                "4pm");
-        priorityChoice.getItems().addAll("High", "Medium", "Low");
-    }
-
+    
+    
     @FXML
     TextField zId;
     @FXML
@@ -72,6 +64,25 @@ public class ConsultationFormFXMLController implements Initializable {
     @FXML
     Button backHome;
     // @FXML CheckBox highbox;    @FXML CheckBox mediumbox;    @FXML CheckBox lowbox;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        timePicked.getItems().addAll("9am", "10am",
+                "11am",
+                "12pm",
+                "1pm",
+                "2pm",
+                "3pm",
+                "4pm");
+        priorityChoice.getItems().addAll("High", "Medium", "Low");
+        backHome.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                gotoHome(e);
+            }
+        });
+    }
+
 
     public Date localDateToUtilDate(LocalDate localDate) {
         GregorianCalendar cal = new GregorianCalendar(
@@ -119,5 +130,17 @@ public class ConsultationFormFXMLController implements Initializable {
         Stage stageedit = (Stage) cancelButton.getScene().getWindow();
         stageedit.close();
 
+    }
+        private void gotoHome(ActionEvent event) {
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("HomeScreen.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Home Screen");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+
+        }
     }
 }
